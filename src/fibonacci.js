c3a1dcfe64e7635e5a2013cbc821574607470142
cdcm <cdcm@pc3-060-l.cs.st-andrews.ac.uk>
2019-04-08 13:12:33 +0100

    Application complete

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,6 +1,37 @@
+/*
+Used memoisation with a recursive fibonacci sequence finding function
+to stop the time complexity from getting out of hand
+*/
+
+var dictionary = [];
+
 function getSequence(n) {
   // TODO: Implement your code to replace the line below
-  return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55].slice(0, n);
+  var sequence = [];
+  
+  for(var i = 1; i < n + 1; i++) {    //im assuming that fib(0) is added somewhere else, so we have to do this instead. 
+    sequence.push(fib(i));
+  }
+  return sequence;
+}
+
+function fib(n) {
+
+  if(n < 2) {
+    return n;
+  }
+
+  if(dictionary[n - 1] == undefined) {    //if it isnt already in the dictionary then calculate it and add it to the dictionary
+    if(n < 2) {
+      dictionary[n - 1] = n;    //0 returns 0, 1 returns 1 
+    } else {
+      dictionary[n - 1] = fib(n - 1) + fib(n - 2);
+    }
+  } else {
+    // console.log("looked up");    //just to show it looking up
+  }
+
+  return dictionary[n - 1];
 }
 
 module.exports = {
--- src/tableFormatter.js
+++ src/tableFormatter.js
@@ -1,9 +1,62 @@
+
+/*
+Function to appropriately pad a given table of numbers. The amount of
+padding after a number for each number is based off of the length (number of digits) of the
+largest number (making the assumption that the largest number is at the end of
+the last array) and the length of the current number. 
+We also need to pad at the beginning which is based off of the size of the largest number in the first
+column.
+
+First we need to find the size of the table so that we can find the values
+we need for the calculations.
+*/
+
 function format(table) {
+  var length = table.length; 
+  var biggestNumber = table[length - 1][length - 1];
+  var biggestFirstNumber = table[length - 1][0];
+  var biggestNumberLength = getIntegerLength(biggestNumber);
+  var biggestFirstNumberLength = getIntegerLength(biggestFirstNumber);
+
   const tableText = table.map(function(row) {
-    return row.join(" ");
+    var rowString = "";   
+    var firstNumberLength = getIntegerLength(row[0]);
+    var firstPad = biggestFirstNumberLength - firstNumberLength;
+
+    if(biggestFirstNumberLength > 1 || biggestNumberLength > 1) rowString += " ";   //this wasnt specified but appears to be a rule
+
+
+    for(var i = 0; i < firstPad; i++) {   //the spaces before each row
+      rowString += " ";
+    }
+
+    for(var col = 0; col < length - 1; col++) { 
+      var nextLen;
+      nextLen = getIntegerLength(row[col + 1]);
+      var paddingAmount = biggestNumberLength + 1 - nextLen;   //the formula for how much padding we need, derived from looking at sample output
+
+      rowString += row[col].toString();  //add the actual number
+
+      for(var count = 0; count < paddingAmount; count++) {  //pad the correct amount
+        rowString +=  " ";
+      }      
+    }
+
+    rowString += row[length - 1]; //add the last number at the end, to avoid getting a null pointer in the loop
+
+    return rowString;
   });
 
   return "\n" + tableText.join("\n") + "\n";
+}
+
+/*
+Find the number of digits in a given int, could alternatively 
+do log10(n) + 1 for speed but I was getting some odd garbage collector
+errors.
+*/
+function getIntegerLength(n) {
+    return n.toString().length;
 }
 
 module.exports = { format };
